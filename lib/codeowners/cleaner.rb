2990a7057d8c3cb12364d3678b13a16fc03a19d9
Bernardo Amorim <bernardo@toptal.com>
2020-06-04 14:08:55 -0300

    refactor: remove in-place mutations in favor of a more functional approach

--- lib/codeowners/cleaner.rb
+++ lib/codeowners/cleaner.rb
@@ -11,34 +11,27 @@ module Codeowners
   # Cleans a codeowner file by sorting, grouping and removing
   # useless patterns, all automatically
   class Cleaner
-    # Get repo metadata and compare with the owners
     def initialize(repo)
       @git = Git.open(repo, log: Logger.new(IO::NULL))
-      @repo_dir = repo
+      @filename = Checker::CodeOwners.filename(repo)
     end
 
     def clean!
-      extract_patterns!
-      normalize_patterns!
-      remove_duplicates!
-      remove_useless!
-      group_and_persist!
-    end
+      patterns =
+        extract_patterns
+        .map { |p| normalized_line(p) }
+        .uniq { |p| line_id(p) }
+        .select { |p| line_pattern_has_files(p) }
 
-    def hash_of_arrays
-      Hash.new { |h, k| h[k] = [] }
-    end
-
-    def pattern_has_files(pattern)
-      @git.ls_files(pattern.gsub(%r{^/}, '')).reject(&whitelist).any?
+      group_and_persist!(patterns)
     end
 
     def file_manager
-      @file_manager ||= Checker::FileAsArray.new(Checker::CodeOwners.filename(@repo_dir))
+      @file_manager ||= Checker::FileAsArray.new(@filename)
     end
 
     def whitelist_filename
-      @whitelist_filename ||= Checker::CodeOwners.filename(@repo_dir) + '_WHITELIST'
+      @whitelist_filename ||= @filename + '_WHITELIST'
     end
 
     def whitelist
@@ -47,32 +40,35 @@ module Codeowners
 
     private
 
-    def extract_patterns!
-      @patterns = Checker::CodeOwners.new(file_manager).main_group.select(&:pattern?)
+    def extract_patterns
+      Checker::CodeOwners.new(file_manager).main_group.select(&:pattern?)
     end
 
-    def normalize_patterns!
-      @patterns.each { |line| line.owners = line.owners.sort }
+    def line_pattern_has_files(line)
+      @git.ls_files(line.pattern.gsub(%r{^/}, '')).reject(&whitelist).any?
     end
 
-    def remove_duplicates!
-      @patterns.uniq! { |line| [line.pattern, *line.owners] }
+    def line_id(line)
+      [line.pattern, *line.owners]
     end
 
-    def remove_useless!
-      @patterns.select! { |line| pattern_has_files(line.pattern) }
+    def normalized_line(line)
+      line = line.clone
+      line.owners = line.owners.sort
+      line
     end
 
-    def group_and_persist!
-      new_file_lines =
-        @patterns
+    def group_and_persist!(lines)
+      file_manager.content = group_and_render(lines)
+      file_manager.persist!
+    end
+
+    def group_and_render(lines)
+      lines
         .group_by(&:owners)
         .sort_by { |owners, _lines| owners }
-        .map { |owners, lines| group_to_string(owners, lines) }
+        .map { |owners, group_lines| group_to_string(owners, group_lines) }
         .join("\n\n")
-
-      file_manager.content = new_file_lines
-      file_manager.persist!
     end
 
     def group_to_string(owners, lines)
